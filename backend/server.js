956398be146ba2115e5d7d9587cb2a40348b90fd
Érick <eierickdasilva@gmail.com>
2024-07-17 17:55:14 -0300

    Arrumando nome do arquivo server.js no package.json

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
     req.addListener('end', function () {
         path.serve(req, res)
     }).resume()
-  }).listen(8080);
+}).listen(8080);
 
 
-  console.log('Server criado em: localhost:8080')
+console.log('Server criado em: localhost:8080');
