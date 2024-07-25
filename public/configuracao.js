18026b225b777aea6fb07b88147662dd1804c0c2
Érick <eierickdasilva@gmail.com>
2024-07-25 20:24:24 -0300

    Corrigindo erro de ortografia

--- public/configuracao.js
+++ public/configuracao.js
@@ -1,16 +1,16 @@
 const checkboxTheme = document.querySelector('.checkbox-theme');
 
-if (localStorage.getItem('cadapio-dark-mode') && localStorage.getItem('cadapio-dark-mode') === 'true') {
+if (localStorage.getItem('cardapio-dark-mode') && localStorage.getItem('cardapio-dark-mode') === 'true') {
     checkboxTheme.checked = true;
     document.documentElement.classList.add('tema-escuro');
 }
 
 checkboxTheme.addEventListener('change', () => {
-    localStorage.setItem('cadapio-dark-mode', checkboxTheme.checked);
+    localStorage.setItem('cardapio-dark-mode', checkboxTheme.checked);
     location.reload();
 });
 
 // Carrega tema escuro
-if (localStorage.getItem('cadapio-dark-mode') && localStorage.getItem('cadapio-dark-mode') === 'true') {
+if (localStorage.getItem('cardapio-dark-mode') && localStorage.getItem('cardapio-dark-mode') === 'true') {
     document.documentElement.classList.add('tema-escuro');
 }
