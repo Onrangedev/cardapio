017ab2d785c6e5feeb940716b9f5b3d18764f817
Érick <eierickdasilva@gmail.com>
2024-07-26 09:55:50 -0300

    Removendo pergunta de turno do aluno

--- frontend/alunos/alunos.js
+++ frontend/alunos/alunos.js
@@ -116,39 +116,6 @@ if (localStorage.getItem('cardapio-dark-mode') && localStorage.getItem('cardapio
     document.documentElement.classList.add('tema-escuro');
 }
 
-// Define o turno do aluno
-if (!localStorage.getItem('cardapio-turno')) {
-    Swal.fire({
-        title: 'Qual é o seu turno?',
-        html: `
-            <select id="macroSelect" class="swal2-select">
-                <option value="">Selecione um turno</option>
-                <option value="matutino">Matutino</option>
-                <option value="vespertino">Vespertino</option>
-            </select>
-        `,
-        confirmButtonText: 'Confirmar',
-        focusConfirm: false,
-        preConfirm: () => {
-            const selectedOption = document.getElementById('macroSelect').value;
-            if (selectedOption) {
-                localStorage.setItem('cardapio-turno', document.querySelector('#macroSelect').value)
-            } else {
-                Swal.showValidationMessage('Você precisa selecionar um Turno!');
-            }
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            const message = `O seu turno foi definido para ${document.querySelector('#macroSelect').value}`;
-            Swal.fire({
-                icon: 'success',
-                title: 'Turno definido!',
-                text: message
-            });
-        }
-    });
-}
-
 let deferredPrompt;
 
 window.addEventListener('beforeinstallprompt', (e) => {
