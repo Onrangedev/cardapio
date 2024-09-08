8198602b4df019d028c551b1be70a642968c45a8
Érick <eierickdasilva@gmail.com>
2024-09-08 10:03:20 -0300

    Adicionando função de zoom na página

--- configuracao/configuracao.js
+++ configuracao/configuracao.js
@@ -23,7 +23,17 @@ checkboxTheme.addEventListener('change', () => {
 });
 
 // Carrega tema escuro
-if (localStorage.getItem('cardapio-dark-mode') && localStorage.getItem('cardapio-dark-mode') === 'true') document.documentElement.classList.add('tema-escuro')
+if (localStorage.getItem('cardapio-dark-mode') && localStorage.getItem('cardapio-dark-mode') === 'true') document.documentElement.classList.add('tema-escuro');
+
+// Carrega o zoom salvo em local storage
+if (localStorage.getItem('cardapio-escala')) alterarZoom(localStorage.getItem('cardapio-escala'));
+
+// Aguarda evento de clique no botão de adicionar escala para aumentar a escala
+document.querySelector('.btn-adicionar-escala').addEventListener('click', () => alterarZoom((Number(document.querySelector('.valor-escala').textContent) + 10) / 100));
+// Aguarda evento de clique no botão de reduzir escala para diminuir a escala
+document.querySelector('.btn-reduzir-escala').addEventListener('click', () => alterarZoom((Number(document.querySelector('.valor-escala').textContent) - 10) / 100));
+// Aguarda evento de clique no valor da escala para retornar ao padrão
+document.querySelector('.valor-escala').addEventListener('click', () => alterarZoom(1));
 
 // Aguarda evento de clique no botão de voltar para voltar a ppagina inicial
 document.querySelector('.back-home').addEventListener('click', () => location.href = '../alunos/index.html');
@@ -45,3 +55,16 @@ document.querySelector('.btn-compartilhar').addEventListener('click', async (e) 
         }, 2000);
     }
 });
+
+// Altera o zoom na página
+function alterarZoom(valor) {
+    if (valor < 0.50 || valor > 3) return;
+    document.body.style.zoom = valor;
+    localStorage.setItem('cardapio-escala', valor);
+    exibirZoom(valor);
+}
+
+// Exibi o valor de zoom no elemento '.valor-escala'
+function exibirZoom(valor) {
+    document.querySelector('.valor-escala').textContent = parseInt(valor*100);
+}
